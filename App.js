4f61cf9c62fc9cd49d80639f878f75ebc9b6e109
David Figueroa <dfigue92@gmail.com>
2018-06-11 00:01:52 -0700

    Added missing app components & pages

--- App.js
+++ App.js
@@ -1,58 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { createSwitchNavigator } from 'react-navigation';
+import store from 'react-native-simple-store';
+import Intro from './app/pages/intro';
+import Dashboard from './app/pages/dashboard';
+import Data from './app/pages/data';
+import Goals from './app/pages/goals';
+import Settings from './app/pages/settings';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+export default SwitchNavigator = createSwitchNavigator({
+  Intro: Intro,
+  Dashboard: Dashboard,
+  Data: Data,
+  Goals: Goals,
+  Settings: Settings,
+  },
+  {
+    initialRouteName: 'Intro'
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+);
